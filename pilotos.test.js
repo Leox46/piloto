5441d6f094c084317ffbf441ba87ca6e6a7be05f
Leox46 <drleonardo46@gmail.com>
2018-01-09 17:20:56 +0100

    Revert "added feature test"

--- pilotos.test.js
+++ pilotos.test.js
@@ -1,41 +1,42 @@
-const root = process.env.SERVER_URL || 'http://127.0.0.1:8080/api/v1' //'https://pilotobasemodel.herokuapp.com/api/v1'
+const root = process.env.SERVER_URL || 'https://assignmentbasemodel.herokuapp.com/api/v1' // http://127.0.0.1:8080/api
 const fetch = require("node-fetch")
-const pilotosRoot = root+'/pilotos'
-const examplePiloto =  {
-    "pilotoId": "59",
-    "name": "Leonardo",
-    "surname": "Dal Ronco",
-    "bike": "Lambretta"
+const assignmentsRoot = root+'/assignments'
+const exampleAssignment =  {
+    "assignmentId": "30",
+    "studentId": "veniam sit proident",
+    "assignment": {"url":"some url"},
+    "assignmentType": "minim",
+    "assignmentValuation": 21
 }
 // importante per il TEST COVERAGE!
 // const server = require('./server');
 
 // helper methods - you can put these  in a separate file if you have many tests file and want to reuse them
 
-const postPilotos = function (newPiloto) {
-    return fetch(pilotosRoot, {
+const postAssignments = function (newAssignment) {
+    return fetch(assignmentsRoot, {
         method: 'POST',
         headers: {
             'Content-Type': 'application/json',
             'Accept': 'application/json'
         },
-        body: JSON.stringify(newPiloto)
+        body: JSON.stringify(newAssignment)
     })
 }
 
-const putPilotos = function (pilotoId, piloto) {
-    return fetch(pilotosRoot+'/'+pilotoId, {
+const putAssignments = function (assignmentId, assignment) {
+    return fetch(assignmentsRoot+'/'+assignmentId, {
         method: 'PUT',
         headers: {
             'Content-Type': 'application/json',
             'Accept': 'application/json'
         },
-        body: JSON.stringify(piloto)
+        body: JSON.stringify(assignment)
     })
 }
 
-const deletePilotos = function (pilotoId) {
-    return fetch(pilotosRoot+'/'+pilotoId, {
+const deleteAssignments = function (assignmentId) {
+    return fetch(assignmentsRoot+'/'+assignmentId, {
         method: 'DELETE',
         headers: {
             'Content-Type': 'application/json',
@@ -45,8 +46,8 @@ const deletePilotos = function (pilotoId) {
 }
 
 
-const getManyPilotos = function () {
-    return fetch(pilotosRoot, {
+const getManyAssignments = function () {
+    return fetch(assignmentsRoot, {
         method: 'GET',
         headers: {
             'Accept': 'application/json'
@@ -54,8 +55,8 @@ const getManyPilotos = function () {
     })
 }
 
-const getOnePiloto = function (pilotoId) {
-    return fetch(pilotosRoot+'/'+pilotoId, {
+const getOneAssignment = function (assignmentId) {
+    return fetch(assignmentsRoot+'/'+assignmentId, {
         method: 'GET',
         headers: {
             'Accept': 'application/json'
@@ -66,40 +67,40 @@ const getOnePiloto = function (pilotoId) {
 
 
 test('basic post and get single element', () => {
-  return postPilotos(examplePiloto)
+  return postAssignments(exampleAssignment)
     .then(postResponse => { return postResponse.json() })
     .then(postResponseJson => {
-      examplePiloto.pilotoId = postResponseJson.pilotoId
-      return getOnePiloto(examplePiloto.pilotoId)
+      exampleAssignment.assignmentId = postResponseJson.assignmentId
+      return getOneAssignment(exampleAssignment.assignmentId)
     })
     .then(getResponse => {return getResponse.json()})
-    .then(jsonResponse => {expect(jsonResponse.pilotoResult).toEqual(examplePiloto.pilotoResult)})
+    .then(jsonResponse => {expect(jsonResponse.assignmentResult).toEqual(exampleAssignment.assignmentResult)})
     //.catch(e => {console.log(e)})
 });
 
 // importante! Mettere la PUT prima della DELETE!
-test('put item by pilotoId - basic response', () => {
-  return putPilotos(examplePiloto.pilotoId, examplePiloto)
+test('put item by assignmentId - basic response', () => {
+  return putAssignments(exampleAssignment.assignmentId, exampleAssignment)
     .then(response => { expect(response.status).toBe(200) })
     //.catch(e => {console.log(e)})
 });
 
-test('delete by pilotoId - basic response', () => {
-  return deletePilotos(examplePiloto.pilotoId)
+test('delete by assignmentId - basic response', () => {
+  return deleteAssignments(exampleAssignment.assignmentId)
     .then(response => { expect(response.status).toBe(200) })
     //.catch(e => {console.log(e)})
 });
 
-test('get all pilotos - basic response', () => {
-  return getManyPilotos()
+test('get all assignments - basic response', () => {
+  return getManyAssignments()
     .then(response => { expect(response.status).toBe(200) })
     //.catch(e => {console.log(e)})
 });
 
 
 /*
-test('delete by pilotoID - item actually deleted', () => {
-  return getOnePiloto(examplePiloto.pilotoId)
+test('delete by assignmentID - item actually deleted', () => {
+  return getOneAssignment(exampleAssignment.assignmentId)
     .then(res => {expect(res.status).toBe(404)})
     //.catch(e => {console.log(e)})
 });
